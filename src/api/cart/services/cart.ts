673467cc6a26667cbe5915db9b292751f812e5d9
HoangDSR <nvh.02021995@gmail.com>
2024-04-10 12:06:13 +0700

    Customs controller for cart

--- src/api/cart/services/cart.ts
+++ src/api/cart/services/cart.ts
@@ -4,4 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::cart.cart');
+export default factories.createCoreService('api::cart.cart', ({ strapi }) => ({
+
+}));
