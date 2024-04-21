3f6af43857bf6b4655f4755f3b903758635dde90
HoangDSR <nvh.02021995@gmail.com>
2024-04-21 20:54:43 +0700

    Sending emails with a lifecycle hook

---
+++ src/api/promotion/lifecycles.ts
@@ -0,0 +1,48 @@
+module.exports = {
+	async afterCreate(event) {
+		// Connected to "Save" button in admin panel
+		const { result } = event;
+
+		try {
+			// Get user from subscription for anouncement
+			const users = await strapi.entityService.findMany(
+				"api::subscriber.subscriber",
+				{
+					filters: {
+						activated: true,
+					},
+				},
+			);
+
+			for (const user of users) {
+				await strapi
+					.plugin("email-designer")
+					.service("email")
+					.sendTemplatedEmail(
+						{
+							// required
+							to: user.email,
+
+							// optional if /config/plugins.js -> email.settings.defaultFrom is set
+							from: "hoang3409@alwaysdata.net",
+
+							// optional if /config/plugins.js -> email.settings.defaultReplyTo is set
+							replyTo: "hoang3409@alwaysdata.net",
+
+							attachments: [],
+						},
+						{
+							// required - Ref ID defined in the template designer (won't change on import)
+							templateReferenceId: 1,
+						},
+						{
+							name_promotion: result.name,
+							promotion_value: result.discount_rate,
+						},
+					);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	},
+};
