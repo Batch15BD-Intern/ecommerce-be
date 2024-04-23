ddbe368dfd6204d566d99a92ae9afc2c98d1b321
HoangDSR <nvh.02021995@gmail.com>
2024-04-23 09:15:38 +0700

    test socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,69 @@
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+	/**
+	 * An asynchronous register function that runs before
+	 * your application is initialized.
+	 *
+	 * This gives you an opportunity to extend code.
+	 */
+	register(/*{ strapi }*/) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+	/**
+	 * An asynchronous bootstrap function that runs before
+	 * your application gets started.
+	 *
+	 * This gives you an opportunity to set up your data model,
+	 * run jobs, or perform some special logic.
+	 */
+	bootstrap({ strapi }) {
+		const io = require("socket.io")(strapi.server.httpServer, {
+			cors: {
+				origin: "http://localhost:3000",
+				methods: ["GET", "POST"],
+				allowedHeaders: [],
+				credentials: true,
+			},
+		});
+
+		io.on("connection", (socket) => {
+			//Listening for a connection from the frontend
+			socket.on("join", ({ username }) => {
+				// Listening for a join connection
+				console.log("user connected");
+				console.log("username is ", username);
+				if (username) {
+					socket.join("group"); // Adding the user to the group
+					socket.emit("welcome", {
+						// Sending a welcome message to the User
+						user: "bot",
+						text: `${username}, Welcome to the group chat`,
+						userData: username,
+					});
+				} else {
+					console.log("An error occurred");
+				}
+			});
+			socket.on("sendMessage", async (data) => {
+				// Listening for a sendMessage connection
+				// const strapiData = {
+				// 	// Generating the message data to be stored in Strapi
+				// 	data: {
+				// 		user: data.user,
+				// 		message: data.message,
+				// 	},
+				// };
+        socket.broadcast.to("group").emit("message", {
+          //Sending the message to the group
+          user: data.username,
+          text: data.message,
+        });
+				// const axios = require("axios");
+				// await axios
+				// 	.post("http://localhost:1337/api/messages", strapiData) //Storing the messages in Strapi
+				// 	.then((e) => {
+						
+				// 	})
+				// 	.catch((e) => console.log("error", e.message));
+			});
+		});
+	},
 };
