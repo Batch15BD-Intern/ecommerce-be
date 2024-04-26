75fa273cf7f005277d1650b1b48102a365e55a95
ziolata <ziolata3@gmail.com>
2024-04-26 14:26:50 +0700

    "Intergration with product_merchant, clear carts after checkout"

--- src/api/product-item/controllers/product-item.ts
+++ src/api/product-item/controllers/product-item.ts
@@ -4,4 +4,21 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product-item.product-item');
+export default factories.createCoreController('api::product-item.product-item', ({ strapi }) => ({
+  async find(ctx) {
+    const result = await strapi.entityService.findMany('api::product-item.product-item', {
+      fields: ['quantity', "price", 'name'],
+      populate: {
+        product: {
+          fields: ["id", "name"]
+        },
+        carts: {
+          fields: ["id"]
+        }
+      },
+
+    })
+    return { data: result };
+  },
+
+}));
